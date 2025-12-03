e244cca0ada1c9a6354b616522fa09041f1fb5eb
Victor <viktorrr398@gmail.com>
2025-12-03 20:14:27 +0400

    Исправление критических замечаний

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -122,8 +122,8 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate  
 		
 		imageView.layer.masksToBounds = true
 		imageView.layer.borderWidth = 8
-		// Используйте ваши цвета, если они определены как UIColor.ypGreen/ypRed
-		imageView.layer.borderColor = isCorrect ? UIColor.green.cgColor : UIColor.red.cgColor
+
+		imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
 		imageView.layer.cornerRadius = 20
 		
 		DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
